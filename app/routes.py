0fa2983899a70b0098809da66fddf8fec19cbb5a
Dennis X <dtx319@gmail.com>
2022-12-13 11:53:33 -0800

    added initial templates

---
+++ app/routes.py
@@ -0,0 +1,59 @@
+from app import app 
+from flask import render_template
+
+#Routes that return JSON
+user_data = {
+    'lucasl' : {
+        'user_id' : 0,
+        'name': 'Lucas',
+        'favoriteColor' : 'blue'
+    },
+    'christophert' : {
+        'user_id' : 1,
+        'name': 'Christopher',
+        'favoriteColor' : 'orange'
+    },
+    'joelt' : {
+        'user_id' : 2,
+        'name': 'Joel',
+        'favoriteColor' : 'red'
+    },
+}
+
+
+
+# Create a route to get all user information
+@app.route('/api/users/')
+def users(): 
+    return user_data
+
+# Create a route to get user information based on their username
+@app.route('/api/user/username/<username>')
+def user(username):
+    if username not in user_data:
+        return 'User not found'
+
+    return user_data[username]
+
+# Create a route to get user information based on their id
+@app.route('/api/user/id/<id>')
+def user_by_id(id):
+    for key, user in user_data.items():
+        if user['user_id'] == int(id):
+            return user
+
+        return 'User not found'
+
+
+# Routes that return/display HTML
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
