7d870cf96fcbfd1401fb64f2216e4c86ac56dd21
Dennis X <dtx319@gmail.com>
2022-12-22 08:40:23 -0800

    more foundational edits to __init__.py and config.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+
+# Connecting our config class and our flask application
+app.config.from_object(Config)
 
 from app import routes
--- config.py
+++ config.py
@@ -1,1 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+# Get the file path to our .env file
+basedir = os.path.abspath(os.path.dirname(__name__))
+
+# Take the basedir, and use it to load in your .env file
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config:
+    FLASK_DEBUG = os.getenv('FLASK_DEBUG')
+    
