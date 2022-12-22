ca50b9962fade80d7052bb384d6c698edcb97599
Dennis X <dtx319@gmail.com>
2022-12-22 14:38:23 -0800

    added form to create post on homepage. also added create_post function in routes.py

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -1,6 +1,7 @@
 from . import bp as app
-from flask import render_template
+from flask import render_template, request, redirect, url_for 
 from app.blueprints.main.models import User, Post
+from app import db
 
 # Routes that return/display HTML
 @app.route('/')
@@ -18,3 +19,15 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/post', methods=['POST'])
+def create_post():
+    post_title = request.form['title']
+    post_body = request.form['body']
+
+    new_post = Post(title=post_title, body=post_body, user_id=1)
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(url_for('main.home'))
