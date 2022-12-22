6e8369f1bcff2b9f4659fc19a13af4c6eb35757e
Dennis X <dtx319@gmail.com>
2022-12-22 15:17:17 -0800

    downloaded flask-login and added it to main.models.py

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -2,6 +2,7 @@ from . import bp as app
 from app.blueprints.main.models import User
 from app import db
 from flask import render_template, request, redirect, url_for
+from flask_login import login_user
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
--- app/blueprints/main/models.py
+++ app/blueprints/main/models.py
@@ -1,7 +1,8 @@
-from app import db
+from app import db, login_manager
+from flask_login import UserMixin
 from datetime import datetime
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True)
     username = db.Column(db.String(50))
@@ -15,3 +16,7 @@ class Post(db.Model):
     body = db.Column(db.String(1200))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
