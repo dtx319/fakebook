19c4c19615ae5c214616726b82a24e24cb56d644
Dennis X <dtx319@gmail.com>
2022-12-22 15:52:02 -0800

    edits to routes.py for registration confirmation

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -1,7 +1,7 @@
 from . import bp as app
 from app.blueprints.main.models import User
 from app import db
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -22,11 +22,11 @@ def login():
     # There was a user, and the password was correct
 
     if user is None:
-        print('There was not a user with that email.')
+        flash('There was not a user with that email.')
     elif user.password != password:
-        print('The password was incorrect.')
+        flash('The password was incorrect.')
     else:
-        print('Logged in successfully')
+        flash('Logged in successfully')
         login_user(user)
         return redirect(url_for('main.home'))
 
@@ -34,11 +34,45 @@ def login():
 
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+
+    email = request.form['inputEmail']
+    username = request.form['inputUsername']
+    first_name = request.form['inputFirstName']
+    last_name = request.form['inputLastName']
+    password = request.form['inputPassword']
+    confirm_password = request.form['inputPasswordConfirm']
+
+    # A user with this email already exists
+    # A user with this email does not exist, but the passwords do not match
+    # A user with this email does not exist, and the passwords match ~ REGISTER THE USER
+
+    check_user = User.query.filter_by(email=email).first()
+
+    if check_user is not None:
+        flash('A user with this email already exists.')
+
+    elif password != confirm_password:
+        flash('The passwords do not match.')
+    
+    else:
+        #REGISTER USER
+        new_user = User(email=email, username=username, first_name = first_name, last_name = last_name, password = password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('User registered successfully', 'success')
+        return redirect(url_for('auth.login'))
+
     return render_template('register.html')
+    
+
+    
 
 @app.route('/logout')
 def logout():
     logout_user()
+    flash('User logged out', 'success')
     return redirect(url_for('auth.login'))
