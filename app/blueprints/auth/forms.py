45f8c4ac6b033c220c54c4b41c9a4c50c334df57
Dennis X <dtx319@gmail.com>
2022-12-22 21:11:04 -0800

    added WTForms data-input and validators

--- app/blueprints/auth/forms.py
+++ app/blueprints/auth/forms.py
@@ -1,6 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
 
-class MyForm(FlaskForm): 
-    name=StringField('first_name', validators=[DataRequired()])
+class SignupForm(FlaskForm): 
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username=StringField('Username', validators=[DataRequired()])
+    first_name=StringField('First Name', validators=[DataRequired()])
+    last_name=StringField('Last Name', validators=[DataRequired()])     
+    password=PasswordField('Password', validators=[DataRequired()])
+    password_confirm=PasswordField('Please confirm your Password', validators=[DataRequired()])
+    submit_button = SubmitField('Create my Account')
+   
--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -3,7 +3,7 @@ from app.blueprints.main.models import User
 from app import db, login_manager
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user
-from forms import MyForm
+from app.blueprints.auth.forms import SignupForm
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -37,42 +37,54 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST' and form.validate_on_submit():
-            first_name = form.name.data
+    form = SignupForm()
 
     if request.method == 'GET':
-        return render_template('register.html')
+        return render_template('register.html', form=form)
 
-    email = request.form['inputEmail']
-    username = request.form['inputUsername']
-    first_name = request.form['inputFirstName']
-    last_name = request.form['inputLastName']
-    password = request.form['inputPassword']
-    confirm_password = request.form['inputPasswordConfirm']
+    try:
+        if request.method == 'POST' and form.validate_on_submit():
+                email = form.email.data
+                username = form.username.data
+                first_name = form.first_name.data
+                last_name = form.last_name.data
+                password = form.password.data
+                password_confirm = form.password_confirm.data
+
+
+
+    # email = request.form['inputEmail']
+    # username = request.form['inputUsername']
+    # first_name = request.form['inputFirstName']
+    # last_name = request.form['inputLastName']
+    # password = request.form['inputPassword']
+    # confirm_password = request.form['inputPasswordConfirm']
 
     # A user with this email already exists
     # A user with this email does not exist, but the passwords do not match
     # A user with this email does not exist, and the passwords match ~ REGISTER THE USER
 
-    check_user = User.query.filter_by(email=email).first()
+                check_user = User.query.filter_by(email=email).first()
 
-    if check_user is not None:
-        flash('A user with this email already exists.', 'danger')
+                if check_user is not None:
+                    flash('A user with this email already exists.', 'danger')
 
-    elif password != confirm_password:
-        flash('The passwords do not match.', 'danger')
-    
-    else:
-        #REGISTER USER
-        new_user = User(email=email, username=username, first_name=first_name, last_name=last_name, password='')
-        new_user.hash_my_password(password)
+                elif password != confirm_password:
+                    flash('The passwords do not match.', 'danger')
+                
+                else:
+                    #REGISTER USER
+                    new_user = User(email=email, username=username, first_name=first_name, last_name=last_name, password='')
+                    new_user.hash_my_password(password)
 
-        db.session.add(new_user)
-        db.session.commit()
-        
-        flash('User registered successfully', 'success')
-        
-        return redirect(url_for('auth.login'))
+                    db.session.add(new_user)
+                    db.session.commit()
+                    
+                    flash('User registered successfully', 'success')
+                    
+                    return redirect(url_for('auth.login'))
+    except:
+        raise Exception("Invalid form data")
 
     return render_template('register.html')
 
