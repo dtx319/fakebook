d31d87cd7c7add9a884cfd2332a26e04b5554678
Dennis X <dtx319@gmail.com>
2022-12-22 21:53:41 -0800

    removed WTForms additions

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -3,7 +3,6 @@ from app.blueprints.main.models import User
 from app import db, login_manager
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user
-from app.blueprints.auth.forms import SignupForm
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -13,6 +12,7 @@ def login():
     # Otherwise, attempt to login the user
     email = request.form['inputEmail']
     password = request.form['inputPassword']
+
 
     user = User.query.filter_by(email=email).first()
 
@@ -31,60 +31,46 @@ def login():
         login_user(user)
         return redirect(url_for('main.home'))
 
+
     return render_template('login.html')
 
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = SignupForm()
-
     if request.method == 'GET':
-        return render_template('register.html', form=form)
+        return render_template('register.html')
 
-    try:
-        if request.method == 'POST' and form.validate_on_submit():
-                email = form.email.data
-                username = form.username.data
-                first_name = form.first_name.data
-                last_name = form.last_name.data
-                password = form.password.data
-                password_confirm = form.password_confirm.data
-
-
-
-    # email = request.form['inputEmail']
-    # username = request.form['inputUsername']
-    # first_name = request.form['inputFirstName']
-    # last_name = request.form['inputLastName']
-    # password = request.form['inputPassword']
-    # confirm_password = request.form['inputPasswordConfirm']
+    email = request.form['inputEmail']
+    username = request.form['inputUsername']
+    first_name = request.form['inputFirstName']
+    last_name = request.form['inputLastName']
+    password = request.form['inputPassword']
+    confirm_password = request.form['inputPasswordConfirm']
 
     # A user with this email already exists
     # A user with this email does not exist, but the passwords do not match
     # A user with this email does not exist, and the passwords match ~ REGISTER THE USER
 
-                check_user = User.query.filter_by(email=email).first()
+    check_user = User.query.filter_by(email=email).first()
 
-                if check_user is not None:
-                    flash('A user with this email already exists.', 'danger')
+    if check_user is not None:
+        flash('A user with this email already exists.', 'danger')
 
-                elif password != confirm_password:
-                    flash('The passwords do not match.', 'danger')
-                
-                else:
-                    #REGISTER USER
-                    new_user = User(email=email, username=username, first_name=first_name, last_name=last_name, password='')
-                    new_user.hash_my_password(password)
+    elif password != confirm_password:
+        flash('The passwords do not match.', 'danger')
+    
+    else:
+        #REGISTER USER
+        new_user = User(email=email, username=username, first_name=first_name, last_name=last_name, password='')
+        new_user.hash_my_password(password)
 
-                    db.session.add(new_user)
-                    db.session.commit()
-                    
-                    flash('User registered successfully', 'success')
-                    
-                    return redirect(url_for('auth.login'))
-    except:
-        raise Exception("Invalid form data")
+        db.session.add(new_user)
+        db.session.commit()
+        
+        flash('User registered successfully', 'success')
+        
+        return redirect(url_for('auth.login'))
 
     return render_template('register.html')
 
